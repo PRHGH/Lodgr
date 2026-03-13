571aef7239d33baa5589097d832c3bda1dcc7e0b
Pamindu <pamindurh@gmail.com>
2026-03-13 10:01:31 +0530

    chore: remove env file import

--- lodgr_backend/src/index.js
+++ lodgr_backend/src/index.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import 'dotenv/config';
 import hotelsRouter from './api/hotel.js';
 import reviewsRouter from './api/review.js';
 import bookingsRouter from './api/booking.js';
